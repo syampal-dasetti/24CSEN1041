54aae4eaf1f87d79b4018fa28d337a7f8d47e421
syampal-dasetti <sdasetti@student.gitam.edu>
2026-01-22 15:41:04 +0530

    Create Voteeligibility.py

---
+++ Voteeligibility.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+
+#Enter your age: 20
+You are eligible to vote.
