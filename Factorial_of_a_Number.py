e076c5c4b33af9793d44e9aff3e748781e979f5e
syampal-dasetti <sdasetti@student.gitam.edu>
2026-01-29 15:19:41 +0530

    Create Factorial_of_a_Number.py

---
+++ Factorial_of_a_Number.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+fact = 1
+
+for i in range(1, num + 1):
+    fact = fact * i
+
+print("Factorial =", fact)
+
+#output
+Enter a number: 5
+Factorial = 120
+
+=== Code Execution Successful ===
